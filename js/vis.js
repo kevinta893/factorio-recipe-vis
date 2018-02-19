e534822da3be363636b5af6882bd56289146d97e
kevinta893 <kevinta893@gmail.com>
2018-02-18 20:02:14 -0700

    Visualization recursion works

--- js/vis.js
+++ js/vis.js
@@ -8,11 +8,35 @@ var chart = null;
 timer = null;
 
 //Config
-var recipeData = "./js/data/recipies.js"
 var nodeWidth = 40;
 var nodePadding = 20;
 var iterations = 32;
 var spread = true;
+
+
+//local
+var currentRecipe = "burner_mining_drill";
+
+
+//Load the recipes database
+var recipes;
+$.getJSON("./js/data/recipes.json", function (json, err){
+	if (err != "success"){
+		console.log("Error cannot load json\n" + err);
+		return;
+	}
+
+	//parse the list into a hashmap
+	var rawList = json;
+	recipes = {};
+	for (var i = 0 ; i < rawList.length ; i++){
+		var recipe = rawList[i];
+		recipes[recipe.id] = recipe;
+	}
+	console.log("Recipes Loaded");
+    updateRecipe();
+});
+
 
 
 d3.selectAll(".controls input").on("change", updateKnobs);
@@ -59,7 +83,6 @@ function updateType() {
 		chart.on("link:"+evt, function(link) { logEvent("link:"+evt, link.source.name+" → "+link.target.name); });
 	});
 	updateKnobs();
-	updateSource();
 }
 
 function numberControl(id, value) {
@@ -87,5 +110,78 @@ function logEvent(name, s) {
 	l.append("span").text(s);
 	e.node().scrollTop = e.node().scrollHeight;
 }
+
+
+
+
+
+function updateRecipe(){
+	//recursively convert a recipe into data that the visualization engine can handle
+	console.log("Loading recipe: " + recipes[currentRecipe].name);
+	var sankeyData = recipeToSankey(currentRecipe);
+    chart.draw(sankeyData);
+}
+
+function recipeToSankey(recipeId){
+	var nodeSet = new Set([]);
+	var recipeSankey = recipeToSankeyRecurse(recipeId, nodeSet, 0);
+
+	//turn all link targets and sources into indicies
+
+	//hashmap each item in the set
+	var nodeList = Array.from(nodeSet);
+	var nodeIndicies = {};
+	for (var i  = 0; i < nodeList.length ; i++){
+        nodeIndicies[nodeList[i]] = i;
+	}
+
+
+	for (var i  = 0; i < recipeSankey.links.length ; i++) {
+        recipeSankey.links[i].source = nodeIndicies[recipeSankey.links[i].source];
+        recipeSankey.links[i].target = nodeIndicies[recipeSankey.links[i].target];
+    }
+
+	return recipeSankey;
+}
+
+function recipeToSankeyRecurse(recipeId, nodeSet, level){
+    var ret = {
+        "nodes" : [],
+        "links" : []
+    }
+
+    var recipeItem = recipes[recipeId];
+
+    ret.nodes.push({"name" : recipeItem.name});
+    nodeSet.add(recipeItem.id);
+
+
+	if (recipeItem.type == "primative"){
+    	return ret;
+	}
+	else{
+    	var recipeItems = recipeItem.recipe0.items;
+    	for (var i = 0; i < recipeItems.length ; i++) {
+            var recipePart = recipeItems[i];
+            ret.links.push({
+                "source": recipeId,
+                "target": recipePart.id,
+                "value": recipePart.amount
+            })
+
+
+            //recurse and combine results
+            var deeperRecipe = recipeToSankeyRecurse(recipePart.id, nodeSet, level + 1);
+            ret.nodes = ret.nodes.concat(deeperRecipe.nodes);
+            ret.links = ret.links.concat(deeperRecipe.links);
+        }
+
+        return ret;
+	}
+
+
+}
+
+
 
 updateType();
